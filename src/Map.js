2328028b819844796fc7c2e43973a8d56ea70b1a
burgonyapure <deejay.spory@gmail.com>
2020-07-30 10:50:45 +0200

    map details

--- src/Map.js
+++ src/Map.js
@@ -14,7 +14,7 @@ class Map extends Component {
         var mymap = L.map('mapid').setView(this.props.pos, 13);
         var marker = L.marker([51.5, -0.09]).addTo(mymap);
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            maxZoom: 20,
+            maxZoom: 18,
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                 '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
                 'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -26,7 +26,7 @@ class Map extends Component {
 
     render (){
         return(
-            <div id="mapid" style={{height:'300px'}}></div>
+            <div id="mapid" style={{height:'700px'}}></div>
         );
     }
 }
